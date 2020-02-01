a739da32057218cec0433e7dccf7a122d144dfef
vincent van der walt <vincent.vanderwalt@zonal.co.uk>
2020-02-01 22:16:33 +0000

    initial commit

---
+++ FunctionTemplate/Src/SampleFunction.Api/Program.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleFunction
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME")))
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            else
+            {
+                var lambdaEntry = new LambdaEntryPoint();
+                var functionHandler = (Func<APIGatewayProxyRequest, ILambdaContext, Task<APIGatewayProxyResponse>>)(lambdaEntry.FunctionHandlerAsync);
+                using var handlerWrapper = HandlerWrapper.GetHandlerWrapper(functionHandler, new JsonSerializer());
+                using var bootstrap = new LambdaBootstrap(handlerWrapper);
+                await bootstrap.RunAsync();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# aws-lambda-dotnetcore-3-template
-dotnet template for using dotnetcore 3 with aws lambda
+# Introduction
+
+This is a template for using dotnetcore 3.1 with aws lambda functions
+
+## Using the Template
+
+### Template installation
+
+To install the template :
+
+- Clone the project locally
+- In the root folder run
+
+```sh
+dotnet new -i .\
+```
+
+After running that, you now have access to a template with a short new of `serverless.DotNetCore3`
+
+### Template usage
+
+Once installed you can now create a new project based on the template.
+
+Navigate to a folder where you want to create the project in.
+
+```sh
+dotnet new serverless.DotNetCore3 -sn {function name}
+```
+
+for example
+
+```sh
+dotnet new serverless.DotNetCore3 -sn sample
+```
+
+This will create a solution and function project for you, including a sample controller and sample unit test
